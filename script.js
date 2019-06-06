0775c2f91f0dc5b2ce677077bfdb45bb2b4f95ba
fsotol69 <fsotol69@gmail.com>
2019-06-07 00:13:27 +0200

    commit alternativo

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ addEventListener('load', () => {
     showCharsetCodeLetters(inputText.value);
     //showLetters(inputText.value);
   });
+  //loadLetter();
 });
-
-function showLetters(text) {
-  flip_flap.innerHTML = '';
-}
 
 function showCharsetCodeLetters(text) {
   let html = '';
   for (let i = 0; i < text.length; i++) {
     flip_flap.innerHTML = '';
   }
-  
+}
+
+function loadLetter() {
+  let back_flip = document.querySelector('[tokenid="1R"]');
+  back_flip.innerHTML = 'A';
+  let front_flip = document.querySelector('[tokenid="1F"]');
+  front_flip.innerHTML = 'B';
 }
