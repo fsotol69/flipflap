5790f8689f551109a9d4efa7aa70166718616b0f
fsotol69 <fsotol69@gmail.com>
2019-06-07 08:44:45 +0200

    commit

--- script.js
+++ script.js
@@ -1,19 +1,81 @@
 let inputText = document.getElementById('input_text');
-let flip_flap = document.getElementById('flip_flap');
+let letters = document.getElementsByClassName('letter');
+let prueba = document.getElementById('prueba');
 
-addEventListener('load', () => {
-  inputText.addEventListener('change', () => {
-    showCharsetCodeLetters(inputText.value);
-    //showLetters(inputText.value);
-  });
-  //loadLetter();
-});
+inputText.onchange = () => {
+  //console.log(inputText.value)
+  //writePrueba(inputText.value)
+  showCharsetCodeLetters(inputText.value.toUpperCase());
+};
+
+// inputText.addEventListener(
+//   'change',
+//   console.log(inputText.value)
+//  // writePrueba(inputText.value)
+//   //showCharsetCodeLetters(inputText.value)
+// );
+
+function writePrueba(text) {
+  console.log('running prueba...');
+  console.log(text.toUpperCase());
+  //prueba.innerHTML = text.toUpperCase();
+}
 
 function showCharsetCodeLetters(text) {
-  let html = '';
+  console.log('running...');
   for (let i = 0; i < text.length; i++) {
-    flip_flap.innerHTML = '';
+    let code = text.charAt(i);
+    console.log(code);
+    flipLetter(text.charAt(i), i);
   }
+}
+
+function flipLetter(value, position) {
+  let letter = letters[position];
+  console.log(letter);
+  if (value !== '') {
+    let initialValue = letter.innerHTML;
+    let finalValue = value;
+    let initCharcode;
+    if (initialValue === undefined || initialValue === '') {
+      initCharcode = 65;
+    } else {
+      initCharcode = initialValue.charCodeAt(0);
+    }
+    let finalCharcode = finalValue.charCodeAt(0);
+    console.log('initcode : ' + initCharcode);
+    console.log('finalcode : ' + finalCharcode);
+    if (initCharcode <= finalCharcode) {
+      for (let code = initCharcode; code <= finalCharcode; code++) {
+        console.log('code : ' + code);
+        console.log(typeof code);
+        let charLetter = String.fromCharCode(code);
+        console.log('character : ' + charLetter);
+        //printLetter(charLetter, letter);
+        (function(index) {
+          setTimeout(function() {
+            printLetter(charLetter, letter);
+          }, 1000);
+        })(code);
+      }
+    } else {
+      for (let code = finalCharcode; code >= finalCharcode; code--) {
+        setTimeout(code => {
+          letter.innerHTML = String.fromCharCode(code);
+        }, 100000);
+      }
+    }
+  } else {
+    letter.innerHTML = '';
+  }
+}
+
+function printLetter(letter, domElement) {
+  // setTimeout(() => {
+  //   console.log('letter : ' + letter);
+    domElement.innerHTML = letter;
+  //   console.log(domElement);
+  // }, 1000);
 }
 
 function loadLetter() {
